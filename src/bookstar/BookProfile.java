45a82e5970927d07ec899abe2cfb5382fa3663e3
072671415 <072671415@109R030573Y8Q22.EDU.YRDSB.NET>
2018-01-17 11:15:35 -0500

    Merge origin/master
    Conflicts:
    	students.txt

--- src/bookstar/BookProfile.java
+++ src/bookstar/BookProfile.java
@@ -201,7 +201,7 @@ public class BookProfile extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         this.setVisible(false);
-        new UserPageScreen().setVisible(true);
+      
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
